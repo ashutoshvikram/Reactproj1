142456220f84824790bba46320f48eda9384b5d2
ashutoshvikram <41039344+ashutoshvikram@users.noreply.github.com>
2020-04-24 12:33:45 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ class Home extends Component{
     constructor(){
         super();
         this.state={
-            loading:true,
+            loading:false,
             items: [],
            
         };
